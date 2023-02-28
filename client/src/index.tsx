ac37ceb9786fa97654d851941fd847dd58e11fa3
Paulo Zaneripe <paulozaneripe@hotmail.com>
2023-02-28 02:29:15 -0300

    Removed useless code

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,5 +6,5 @@ import '@src/assets/styles/global.css';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
